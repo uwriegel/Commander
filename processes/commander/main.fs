c715cfdfcd8459a2bebd17dfd86acf89da0f4e20
Uwe Riegel <uwriegel@gmail.com>
2017-11-19 14:45:06 +0100

    launch web server with parameters

--- processes/commander/main.fs
+++ processes/commander/main.fs
@@ -1,18 +1,23 @@
 open Configuration
 open Server
 
-printfn "Starting Test Server"
-let configuration = Configuration.create {
-        Configuration.createEmpty() with 
-            Port = 20000; 
-            WebRoot = "/home/uwe/Projekte/Commander/" 
-    }
+[<EntryPoint>]
+let main args =
+    printfn "Starting Test Server"
+    let configuration = Configuration.create {
+            Configuration.createEmpty() with 
+                Port = 20000; 
+                WebRoot = args.[0]
+        }
 
-try
-    let server = create configuration
-    server.start ()
-    stdin.ReadLine() |> ignore
-    server.stop ()
-with
-    | ex -> printfn "Fehler: %O" ex
-
+    try
+        let server = create configuration
+        server.start ()
+        stdin.ReadLine() |> ignore
+        server.stop ()
+        0
+    with
+        | ex -> 
+            printfn "Fehler: %O" ex
+            -1
+    
