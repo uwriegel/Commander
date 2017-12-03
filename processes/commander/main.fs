224bf56935976b93749883155a42e43082fa2a2d
Uwe Riegel <uwriegel@gmail.com>
2017-12-03 21:29:46 +0100

    Detect platform in f#

--- processes/commander/main.fs
+++ processes/commander/main.fs
@@ -2,6 +2,7 @@ open Server
 open Request
 open System.Threading
 open System.Diagnostics
+open System.Runtime.InteropServices
 
 // TODO: Portscanner
 // Ubuntu Theme
@@ -17,9 +18,12 @@ let configuration = Configuration.create {
 ThreadPool.SetMinThreads(60, 60) |> ignore
 
 try
+    let os = RuntimeInformation.OSDescription 
+    let windows = os.StartsWith "Microsoft"
     let server = create configuration { asyncRequest = asyncRequest; onNewWebSocket = onNewWebSocket }
     server.start ()
-    let info = ProcessStartInfo (@"google-chrome", "--app=http://localhost:20000/commander.html")
+    let chrome = if windows then @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" else "google-chrome"
+    let info = ProcessStartInfo (chrome, "--app=http://localhost:20000/commander.html")
     info.UseShellExecute <- true
     let p = new Process()
     p.EnableRaisingEvents <- true
