c0a5edc89c73cdfd2c4c446b6be4eb78b6d4e63c
Uwe Riegel <uwriegel@gmail.com>
2017-12-07 22:51:46 +0100

    New Configuration and Favicon

--- processes/commander/main.fs
+++ processes/commander/main.fs
@@ -5,7 +5,6 @@ open System.Diagnostics
 open System.Runtime.InteropServices
 open System.Net.Sockets
 
-// TODO: fawikon
 // TODO: GetDrives Linux mit DVD
 
 printfn "Starting Test Server"
@@ -21,18 +20,23 @@ let rec checkPort port =
 let port = checkPort 20000
 printfn "using port %d" port
 
+let os = RuntimeInformation.OSDescription 
+let windows = os.StartsWith "Microsoft"
+
+
 let configuration = Configuration.create {
         Configuration.createEmpty() with 
             Port = port 
             WebRoot = "."
+            asyncRequest = asyncRequest
+            onNewWebSocket = onNewWebSocket
+            favicon = if windows then "assets\images\kirk2.png" else "assets/images/kirk2.png"
     }
 
 ThreadPool.SetMinThreads(60, 60) |> ignore
 
 try
-    let os = RuntimeInformation.OSDescription 
-    let windows = os.StartsWith "Microsoft"
-    let server = create configuration { asyncRequest = asyncRequest; onNewWebSocket = onNewWebSocket }
+    let server = create configuration 
     server.start ()
     let chrome = if windows then @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" else "google-chrome"
     let info = ProcessStartInfo (chrome, sprintf "--app=http://localhost:%d/commander.html" <| port)
