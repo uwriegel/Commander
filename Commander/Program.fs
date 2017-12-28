3538a877fe3d57382786d20882218cf97df351fd
Uwe Riegel <uwriegel@gmail.com>
2017-12-28 17:05:27 +0100

    WebServer started in Chrome

--- Commander/Program.fs
+++ Commander/Program.fs
@@ -1,23 +1,15 @@
 ﻿open System.Diagnostics
 open System.Net.Sockets
 open System.Runtime.InteropServices
+open Starter;
 
 [<DllImport("hook.dll")>]
 extern void Start()
 
 printfn "Starting Commander service"
 
-let rec checkPort port = 
-    try
-        let listener = IPV6ListenerFactory.create port
-        listener.Listener.Start()
-        listener.Listener.Stop()
-        port
-    with
-    | _ -> checkPort <| port + 1
- 
-
 let port = checkPort 20000
+
 printfn "using port %d" port
 
 let info = ProcessStartInfo (@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", sprintf "--app=http://localhost:%d/commander.html" <| port)
--- Commander/Starter.fs
+++ Commander/Starter.fs
@@ -1,6 +1,37 @@
 ﻿module Starter
 
+open System.Threading
+open Server
+
+let rec checkPort port = 
+    try
+        let listener = IPV6ListenerFactory.create port
+        listener.Listener.Start()
+        listener.Listener.Stop()
+        port
+    with
+    | _ -> checkPort <| port + 1
+
 let start(path: string, hwnd: int64) =
+    let port = checkPort 20000
+
+    let configuration = Configuration.create {
+        Configuration.createEmpty() with 
+            Port = port 
+            WebRoot = @"C:\Users\urieg\Documents\Projekte\Commander" // TODO: anhand des Modulpaths berechnen
+            //asyncRequest = asyncRequest
+            //onNewWebSocket = onNewWebSocket
+            favicon = "assets\images\kirk2.png"
+    }
+
+    ThreadPool.SetMinThreads(60, 60) |> ignore
+
+    try
+        let server = create configuration 
+        server.start ()
+    with
+        | ex -> 
+            printfn "Fehler: %O" ex
     ()
 
 
